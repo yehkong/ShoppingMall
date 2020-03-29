235b3816b20b025b5ba9503b76dc02cfcee4c222
yehkong <yehkong@126.com>
2020-03-29 22:30:46 +0800

    fix some bugs

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/manager/service/impl/TestServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/manager/service/impl/TestServiceImpl.java
@@ -1,10 +1,11 @@
 package com.taotao.manager.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.taotao.manager.mapper.TestMapper;
 import com.taotao.manager.service.TestService;
-
+@Service
 public class TestServiceImpl implements TestService {
 
 	@Autowired
